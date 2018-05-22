1fbf4f5aaea35ff9f29615b5535a588e9b813c97
Pablo Ribalta <pribalta@future-processing.com>
2018-05-22 14:20:03 +0200

    Added executor

--- pyPso/pso.py
+++ pyPso/pso.py
@@ -1,11 +1,132 @@
+from typing import List
+from multiprocessing import Pool
+
 import numpy as np
 
-from pyPso.swarm import Particle
+
+class ObjectiveFunctionBase(object):
+    def __call__(self, *args, **kwargs):
+        pass
+
+
+class PsoParameters(object):
+    def __init__(self,
+                 omega: float = 0.5,
+                 phip: float = 0.5,
+                 phig: float = 0.5):
+        self._omega = omega
+        self._phip = phip
+        self._phig = phig
+
+    def omega(self):
+        return self._omega
+
+    def phip(self):
+        return self._phip
+
+    def phig(self):
+        return self._phig
+
+
+class Particle(object):
+    def __init__(self,
+                 lower_bound: np.ndarray,
+                 upper_bound: np.ndarray,
+                 parameters: PsoParameters):
+        assert lower_bound.dtype == upper_bound.dtype, \
+            "Upper and lower bound must share the same type. Found {} and {}".format(lower_bound.type,
+                                                                                     upper_bound.type)
+        assert np.greater_equal(upper_bound, lower_bound), \
+            "Upper bound values must be greater or equal than lower bound values. Received {} and {}".format(
+                upper_bound,
+                lower_bound)
+
+        self._lower_bound = lower_bound
+        self._upper_bound = upper_bound
+        self._parameters = parameters
+
+        self._position = [self._initialize_position()]
+        self._velocity = [self._initialize_velocity()]
+        self._score = []
+
+    def position(self) -> np.ndarray:
+        return self._position[-1]
+
+    def velocity(self) -> np.ndarray:
+        return self._velocity[-1]
+
+    def best_position(self) -> np.ndarray:
+        return self._position[np.argsort(self._score)[-1]]
+
+    def best_score(self) -> np.ndarray:
+        return self._score[np.argsort(self._score)[-1]]
+
+    def update_velocity(self,
+                        swarm_best: np.ndarray):
+        assert self._score, "Cannot update velocity while scores are empty. Evaluate first."
+
+        rp, rg = self._initialize_random_coefficients()
+
+        self._velocity.append(self._parameters.omega() * self.velocity()
+                              + self._parameters.phip() * rp * (self.best_position() - self.position())
+                              + self._parameters.phig() * rg * (swarm_best - self.position()))
+        self._position.append(self._update_position())
+
+    def update_score(self, score: float) -> None:
+        self._score.append(score)
+
+    def _initialize_position(self) -> np.ndarray:
+        return np.array([np.random.uniform(low, high)
+                         for low, high in zip(self._lower_bound,
+                                              self._upper_bound)]).astype(self._lower_bound.dtype)
+
+    def _initialize_velocity(self) -> np.ndarray:
+        return np.array([np.random.uniform(-(high - low), high - low)
+                         for low, high in zip(self._lower_bound,
+                                              self._upper_bound)]).astype(self._lower_bound.dtype)
+
+    def _initialize_random_coefficients(self):
+        return np.random.uniform(0, 1), np.random.uniform(0, 1)
+
+    def _update_position(self):
+        new_position = self._position[-1] + self._velocity[-1]
+
+        for i in range(new_position.size):
+            if self._lower_bound[i] > new_position[i]:
+                new_position[i] = self._lower_bound[i]
+            elif self._upper_bound < new_position[i]:
+                new_position[i] = self._upper_bound[i]
+
+        return new_position
+
+
+class Executor(object):
+    def __init__(self,
+                 objective_function: ObjectiveFunctionBase,
+                 threads: int,
+                 minimum_improvement: float,
+                 minimum_step: float):
+        assert threads > 0, "Number of threads must be greater than zero"
+        self._objective_function = objective_function
+        self._threads = threads
+        self._minimum_improvement = minimum_improvement
+        self._minimum_step = minimum_step
+
+    def calculate_scores(self,
+                         particles: List[Particle]):
+        with Pool(min(self._threads, len(particles))) as pool:
+            return pool.starmap(self._objective_function, zip(particles), chunksize=1)
+
+    def still_improving(self) -> bool:
+        return True
+
+    def still_moving(self) -> bool:
+        return True
 
 
 class Pso(object):
     def __init__(self,
-                 objective_function,
+                 objective_function: ObjectiveFunctionBase,
                  lower_bound: np.ndarray,
                  upper_bound: np.ndarray,
                  swarm_size: int,
@@ -15,22 +136,41 @@ class Pso(object):
                  maximum_iterations: int = 100,
                  minimum_step: float = 10e-8,
                  minimum_improvement: float = 10e-8,
-                 cpu_threads: int = 1,
-                 gpu_threads: int = 1,
+                 threads: int = 1,
                  verbose: bool = False):
 
         assert lower_bound.shape == upper_bound.shape, \
             "Lower and upper bound must have the same shape. Received {} and {}".format(lower_bound.shape,
                                                                                         upper_bound.shape)
 
-        self._particles = [Particle(lower_bound, upper_bound) for _ in range(swarm_size)]
+        assert swarm_size > 0, "Swarm size must be greater than zero"
+        assert 1 >= omega >= 0, "Value for omega should be [0.0, 1.0]"
+        assert 1 >= phip >= 0, "Value for phip should be [0.0, 1.0]"
+        assert 1 >= phig >= 0, "Value for phig should be [0.0, 1.0]"
+        assert maximum_iterations > 0, "Maximum number of iterations must be greater than zero"
+
+        self._parameters = PsoParameters(omega, phip, phig)
+        self._particles = [Particle(lower_bound, upper_bound, self._parameters) for _ in range(swarm_size)]
+        self._maximum_iterations = maximum_iterations
+
+        self._executor = Executor(objective_function,
+                                  threads,
+                                  minimum_improvement,
+                                  minimum_step)
+
+    def run(self):
+        for i in range(self._maximum_iterations):
+            self._executor.calculate_scores(self._particles)
+
+            if not self._executor.still_improving() \
+                    or not self._executor.still_moving():
+                break
 
 
 def main():
-    pso = Pso(None, np.array([1,1,1]), np.array([10,10,10]), 1)
+    pso = Pso(None, np.array([1, 1, 1]), np.array([10, 10, 10]), 1)
     pass
+
 
 if __name__ == '__main__':
     main()
-
-
--- test/test_pso.py
+++ test/test_pso.py
@@ -1,13 +1,172 @@
 import unittest
+from unittest.mock import patch
 
 import numpy as np
 
-from pyPso.pso import Pso
+from pyPso.pso import PsoParameters, Particle
+from pyPso.pso import Pso, ObjectiveFunctionBase, Executor
 
+
+class DummyObjectiveFunction(ObjectiveFunctionBase):
+    def __init__(self):
+        self._ret = 0.0
+
+    def __call__(self, particle: Particle, *args, **kwargs):
+        self._ret += 1.0
+        return self._ret
 
 class TestPso(unittest.TestCase):
 
     def test_that_assert_is_thrown_if_boundaries_are_not_the_same_length(self):
         with self.assertRaises(Exception):
-            Pso(None, np.array([1, 1]), np.array([10, 10, 10]), 1)
+            Pso(DummyObjectiveFunction(), np.array([1, 1]), np.array([10, 10, 10]), 1)
+
+    def test_that_assert_is_thrown_if_max_iters_are_zero(self):
+        with self.assertRaises(Exception):
+            Pso(DummyObjectiveFunction(), np.array([1, 1]), np.array([10, 10]), 1, 0)
+
+    def test_that_assert_is_thrown_if_max_iters_are_less_than_zero(self):
+        with self.assertRaises(Exception):
+            Pso(DummyObjectiveFunction(), np.array([1, 1]), np.array([10, 10]), 1, -10)
+
+    def test_that_assert_is_thrown_if_swarm_size_is_zero(self):
+        with self.assertRaises(Exception):
+            Pso(DummyObjectiveFunction(), np.array([1, 1]), np.array([10, 10]), 0, 1)
+
+    def test_that_assert_is_thrown_if_swarm_size_is_less_than_zero(self):
+        with self.assertRaises(Exception):
+            Pso(DummyObjectiveFunction(), np.array([1, 1]), np.array([10, 10]), -1, 1)
+
+class TestExecutor(unittest.TestCase):
+
+    def test_that_number_of_threads_cannot_be_zero(self):
+        with self.assertRaises(Exception):
+            Executor(DummyObjectiveFunction(), 0, 0, 0)
+
+    def test_that_number_of_threads_is_greater_than_zero(self):
+        with self.assertRaises(Exception):
+            Executor(DummyObjectiveFunction(), -1, 0, 0)
+
+    def test_that_objective_function_is_called(self):
+        ex = Executor(DummyObjectiveFunction(), 1, 0, 0)
+        score = ex.calculate_scores([Particle(np.array([2.0]), np.array([5.0]), PsoParameters())])
+
+        self.assertEqual(len(score), 1)
+        self.assertEqual(score[0], 1.0)
+
+    def test_that_executor_returns_various_results(self):
+        ex = Executor(DummyObjectiveFunction(), 1, 0, 0)
+        score = ex.calculate_scores([Particle(np.array([2.0]), np.array([5.0]), PsoParameters()),
+                                     Particle(np.array([2.0]), np.array([5.0]), PsoParameters())])
+
+        self.assertEqual(len(score), 2)
+        self.assertEqual(score[0], 1.0)
+        self.assertEqual(score[1], 1.0)
+
+class TestParticle(unittest.TestCase):
+
+    def test_that_assert_is_thrown_if_boundaries_are_not_the_same_type(self):
+        with self.assertRaises(Exception):
+            Particle(np.array([1]), np.array([2.0]), PsoParameters())
+
+    def test_that_upper_bound_must_be_greater_or_equal_than_lower_bound(self):
+        Particle(np.array([2]), np.array([2]), PsoParameters())
+        Particle(np.array([2]), np.array([3]), PsoParameters())
+
+        with self.assertRaises(Exception):
+            Particle(np.array([3]), np.array([2]), PsoParameters())
+
+    def test_that_position_is_float_when_bounds_are_float(self):
+        particle = Particle(np.array([2.0]), np.array([5.0]), PsoParameters())
+
+        self.assertEqual(particle.position().dtype, np.array([2.0]).dtype)
+
+    def test_that_position_is_int_when_bounds_are_int(self):
+        particle = Particle(np.array([2]), np.array([5]), PsoParameters())
+
+        self.assertEqual(particle.position().dtype, np.array([2]).dtype)
+
+    def test_that_velocity_is_float(self):
+        particle = Particle(np.array([2.0]), np.array([5.0]), PsoParameters())
+
+        self.assertEqual(particle.velocity().dtype, np.array([2.0]).dtype)
+
+    @patch.object(Particle, '_initialize_position')
+    @patch.object(Particle, '_initialize_velocity')
+    def test_that_position_and_velocity_are_initialized_when_creating_a_particle(self, _initialize_velocity, _initialize_position):
+        Particle(np.array([2.0]), np.array([5.0]), PsoParameters())
+
+        self.assertTrue(_initialize_position.called)
+        self.assertTrue(_initialize_position.called)
+
+    def test_that_position_and_velocity_are_of_length_one_on_creation(self):
+        particle = Particle(np.array([2.0]), np.array([5.0]), PsoParameters())
+
+        self.assertTrue(len(particle._position), 1)
+        self.assertTrue(len(particle._velocity), 1)
+
+    def test_that_score_is_empty_on_creation(self):
+        particle = Particle(np.array([2.0]), np.array([5.0]), PsoParameters())
+
+        self.assertEqual(len(particle._score), 0)
+
+    def test_that_velocity_cannot_be_updated_before_calculating_fitness(self):
+        particle = Particle(np.array([2.0]), np.array([5.0]), PsoParameters())
+
+        with self.assertRaises(Exception):
+            particle.update_velocity(None)
+
+    def test_that_velocity_can_be_updated_after_calculating_fitness(self):
+        particle = Particle(np.array([2.0]), np.array([5.0]), PsoParameters())
+
+        particle.update_score(1.0)
+
+        particle.update_velocity(np.array([3.0]))
+
+    @patch.object(Particle, '_update_position')
+    def test_that_updating_velocity_updates_position(self, mock):
+        particle = Particle(np.array([2.0]), np.array([5.0]), PsoParameters())
+
+        particle.update_score(1.0)
+
+        particle.update_velocity(np.array([3.0]))
+
+        self.assertTrue(mock.called)
+
+    @patch.object(Particle, '_initialize_random_coefficients')
+    def test_that_random_coefficients_are_calculated_when_updating_velocity(self, mock):
+        mock.return_value = (0.5, 0.5)
+        particle = Particle(np.array([2.0]), np.array([5.0]), PsoParameters())
+
+        particle.update_score(1.0)
+
+        particle.update_velocity(np.array([3.0]))
+
+        self.assertTrue(mock.called)
+
+    def test_position_returns_the_latest_position(self):
+        particle = Particle(np.array([2.0]), np.array([5.0]), PsoParameters())
+
+        particle._position = range(0, 10)
+        self.assertEqual(particle.position(), 9)
+
+    def test_velocity_returns_the_latest_position(self):
+        particle = Particle(np.array([2.0]), np.array([5.0]), PsoParameters())
+
+        particle._velocity = range(0, 10)
+        self.assertEqual(particle.velocity(), 9)
+
+    def test_best_position_returns_the_best_position(self):
+        particle = Particle(np.array([2.0]), np.array([5.0]), PsoParameters())
+
+        particle._position = range(10, 20)
+        particle._score = range(0, 10)
+        self.assertEqual(particle.best_position(), 19)
+
+    def test_best_score_returns_the_best_score(self):
+        particle = Particle(np.array([2.0]), np.array([5.0]), PsoParameters())
+
+        particle._score = range(0, 10)
+        self.assertEqual(particle.best_score(), 9)
+
 
