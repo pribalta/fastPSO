4ec618ade51eaa103b1039c317716c1034a537d2
Pablo Ribalta <pribalta@kplabs.com>
2018-05-30 19:12:57 +0200

    Rename package

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from distutils.core import setup
 
 setup(
-    name='pyPso',
+    name='fastPso',
     version='0.9',
     author='Pablo Ribalta Lorenzo',
     author_email='pribalta@ieee.org',
-    packages=['pyPso', 'test'],
-    url='https://github.com/pribalta/pyPSO',
+    packages=['fastPso', 'test'],
+    url='https://github.com/pribalta/fastPSO',
     license='LICENSE.txt',
     description='Fast parallel PSO library for Python with support for CPU and GPU multithreading.',
     long_description=open('README.md').read(),
